a3fce08b61cb3461b3905ea1192836a37f624f50
mihaiastefanoaei <mihai.astefanoaei@bitstone.eu>
2024-07-15 10:01:33 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+# User Management System
+A CRUD Application built using Nodejs, Express and MongoDB
+
+
+## Tech stack
+
+## Screens
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./views/**/*.{html,js,ejs}"],
+  darkMode:false,
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
